a387a0ead936588320dd9a01a19060fe87ab45c3
unknown <>
2016-03-17 23:00:13 +0900

    Move Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+    private Vector2 pos;
+
+    public void Init()
+    {
+        pos = transform.position;
+    }
+
+    public void Updated(float move_pos)
+    {
+        transform.Translate(Vector2.left * move_pos);
+
+        if(transform.position.x <= -25.60f)
+            transform.position = new Vector2(25.50f,.0f);
+    }
+	
+}
---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+
+    private static BackgroundManager instance;
+    public static BackgroundManager Instance { get { return instance; } }
+
+    [SerializeField] private Background[]   background;
+    [SerializeField] private float          speed;
+    [SerializeField] private float          move_pos;
+    [SerializeField] private float          move_dis;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        instance = this;
+        move_pos = .0f;
+        move_dis = .0f;
+
+        for(int i=0; i<2;++i)
+        {
+            background[i].Init();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    move_pos = speed * Time.smoothDeltaTime;
+
+        for(int i=0;i<2;++i)
+        {
+            background[i].Updated(move_pos);
+        }
+
+        move_dis += move_pos;
+	}
+}
