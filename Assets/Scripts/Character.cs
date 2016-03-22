f43ef16dce16498b14a4bb5f60748db473a3d46d
unknown <>
2016-03-22 19:45:52 +0900

    button&jump&doublejump&rideroap

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,8 +8,12 @@ public class Character : MonoBehaviour {
     private Transform       my_transform; 
     private Rigidbody2D     my_rig;
     private Animator        my_animator;
-    private bool            grounded;
 
+    private int             jump_count;
+    private bool            grounded;
+    private bool            is_rideroap;
+
+    [SerializeField]private float     jump_force;
     [SerializeField]private Transform check_ground;
     [SerializeField]private LayerMask ground;
 
@@ -19,6 +23,8 @@ public class Character : MonoBehaviour {
         my_transform = GetComponent<Transform>();
         my_rig = GetComponent<Rigidbody2D>();
         my_animator = GetComponent<Animator>();
+        is_rideroap = false;
+        jump_count = 2;
         
 	}
 	
@@ -26,23 +32,59 @@ public class Character : MonoBehaviour {
 	void Update () {
 	    grounded =Physics2D.OverlapCircle(check_ground.position, 0.1f, 1 << LayerMask.NameToLayer("Ground"));
 
-        if(grounded)
+        if(grounded && my_rig.velocity.y <0)
         {
+            jump_count = 2;
+            character_state = Character_State.RUN;
             my_animator.SetBool("is_jump",false);
-        }
-        if(Input.GetKeyDown(KeyCode.Space)&&grounded)
-            Jump();
-        if(Input.GetKeyDown(KeyCode.Space)&&!grounded)
-        {
-            Jump();
         }
 	}
 
 
-    private void Jump()
+    public void Jump()
     {
-        my_rig.AddForce(Vector2.up * 400);
-        my_animator.SetBool("is_jump", true);
+        if(jump_count>0)
+        {
+            jump_count--;
+            my_rig.AddForce(Vector2.up * jump_force);
+            character_state = Character_State.JUMP;
+            my_animator.SetBool("is_jump", true);
+        }
     }
+
+    private void Ride_roap()
+    {
+        my_rig.gravityScale = 0.0f;
+        my_rig.velocity = Vector2.zero;
+        character_state = Character_State.ROAP;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Roap"))
+        {
+            Ride_roap();
+            is_rideroap = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.CompareTag("Roap"))
+        {
+            leave_roap();
+        }
+    }
+
+    public void leave_roap()
+    {
+        if(is_rideroap)
+        {
+            my_rig.gravityScale = 2.5f;
+            my_rig.AddForce(Vector2.down * jump_force);
+            is_rideroap = false;
+        }
+    }
+
+
 
 }
